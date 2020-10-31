7d2447221d48f3647876bdb3b4d4b2ab88527ff1
Eran Amarante <eran.amarante@yahoo.com.ph>
2020-11-01 00:18:15 +0800

    update packages

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useRoutes } from 'react-router-dom';
+// import routes from './routes';
+import { Button } from 'reactstrap';
+
+const App = () => {
+  // const routing = useRoutes(routes);
+
+  return <Button color="primary">primary</Button>;
+};
+
+export default App;
